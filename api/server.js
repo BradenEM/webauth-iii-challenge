65471857a210ce04b52002cd6c15f7bad9808ddc
Braden Mason <bradenemason@gmail.com>
2019-08-27 17:35:21 -0500

    logging in generates user's token

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const secrets = require("../config/secrets");
 
 const Users = require("../users/users-model");
 
@@ -26,5 +27,33 @@ server.post("/api/register", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+server.post("/api/login", async (req, res) => {
+  let { username, password } = req.body;
+  const user = await Users.findBy({ username }).first();
+
+  try {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      token = genToken(user);
+      res.status(200).json({ message: `Welcome ${user.username}`, token });
+    } else {
+      res.status(401).json({ message: "You shall not pass." });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+function genToken(user) {
+  const payload = {
+    subject: "user",
+    username: user.username
+  };
+  const secret = secrets.jwtSecret;
+  const options = {
+    expiresIn: "1hr"
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = server;
